bbd5b4e25e92d255c251f647a6f72bed5d8344b1
Anusha Kulkarni <88369729+hi-anusha@users.noreply.github.com>
2022-09-05 18:10:04 +0530

    Time: 12 ms (43.18%), Space: 13.9 MB (9.66%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,10 +1,53 @@
-class Solution {
+class Solution
+{
 public:
-    int minCostClimbingStairs(vector<int>& cost) {
-        for(int i=2;i<cost.size();i++)
+    
+    int f1(int n,vector<int>& cost,vector<int>& dp)
+    {
+        if(n==cost.size())
         {
-            cost[i]+=min(cost[i-1],cost[i-2]);
+            return 0;
         }
-        return min(cost[cost.size()-1],cost[cost.size()-2]);
+        
+        if(dp[n]!=-1)
+            return dp[n];
+        
+        int left=f1(n+1,cost,dp)+abs(cost[n]);
+        int right=INT_MAX;
+        if(n<cost.size()-1)
+        {
+            right=f1(n+2,cost,dp)+abs(cost[n]);
+        }
+        return dp[n]= min(left,right);
+    }
+    
+       int f2(int n,vector<int>& cost,vector<int>& dp)
+    {
+        if(n==cost.size())
+        {
+            return 0;
+        }
+         
+           if(dp[n]!=-1)
+            return dp[n];
+        
+           
+        int left=f2(n+1,cost,dp)+abs(cost[n]);
+        int right=INT_MAX;
+        if(n<cost.size()-1)
+        {
+            right=f2(n+2,cost,dp)+abs(cost[n]);
+        }
+        
+           return dp[n]= min(left,right);
+    }
+    
+    int minCostClimbingStairs(vector<int>& cost) 
+    {
+        vector<int> dp(cost.size(),-1);
+        int x=f1(0,cost,dp);
+        int y=f2(1,cost,dp);
+        
+        return min(x,y);
     }
 };
