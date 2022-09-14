cafe7a564043812a30bdf675b5f05a72edc997df
Anusha Kulkarni <88369729+hi-anusha@users.noreply.github.com>
2022-09-14 14:15:06 +0530

    Time: 566 ms (5.09%), Space: 49.5 MB (13.40%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -1,36 +1,40 @@
 class Solution {
 public:
     
-    static bool cmp(const vector<int> &a,const vector<int> &b) 
-    { 
-	    return a[1]>b[1]; 
-    } 
+    static bool comp(vector<int> a,vector<int> b)
+    {
+        return a[1] > b[1];
+    }
     
-    int i;
     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) 
     {
-        sort(boxTypes.begin(), boxTypes.end(), cmp); 
-        int mu=0;
-        for(i=0;i<boxTypes.size() && truckSize>boxTypes[i][0];i++)
+        sort(boxTypes.begin(), boxTypes.end(),comp);
+        
+        for(int i=0;i<boxTypes.size();i++)
         {
-            mu=mu+(boxTypes[i][0]*boxTypes[i][1]);
-            truckSize=truckSize-boxTypes[i][0];
+            cout<<boxTypes[i][0]<<" "<<boxTypes[i][1]<<"\n";
         }
         
-        cout<<mu;
-        cout<<i;
+        int n=0;
+        int u=0;
         
-        int j=i;
-        if(truckSize!=0)
+        for(int i=0;i<boxTypes.size();i++)
         {
-            while(j<boxTypes.size() && truckSize>0)
+            if(n+boxTypes[i][0]<truckSize)
             {
-                mu=mu+(boxTypes[j][1]);
-            truckSize--;
+                n=n+boxTypes[i][0];
+                u=u+(boxTypes[i][1]*boxTypes[i][0]);
+                
+            }
+            else
+            {
+                int remain=truckSize-n;
+                u=u+(boxTypes[i][1]*remain);
+                break;
             }
         }
-                       
-        return mu;
+        
+        return u;
         
     }
 };
