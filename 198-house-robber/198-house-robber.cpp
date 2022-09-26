eafea8225e6dfb6c1251660349d623f7178caf1f
Anusha Kulkarni <88369729+hi-anusha@users.noreply.github.com>
2022-09-26 20:23:29 +0530

    Time: 9 ms (8.11%), Space: 7.7 MB (49.65%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -4,15 +4,14 @@ public:
     int f(vector<int>& nums,int ind,vector<int>& dp)
     {
         int pick=0,dpp=0;
-        if(ind==0)
-            return nums[ind];
+        
         if(ind<0)
             return 0;
         if(dp[ind]!=-1)
             return dp[ind];
-        if(ind>=1)
+       
         pick=nums[ind]+f(nums,ind-2,dp);
-        if(ind>=0)
+       
          dpp=0+f(nums,ind-1,dp);
         return dp[ind]=max(pick,dpp);
     }
