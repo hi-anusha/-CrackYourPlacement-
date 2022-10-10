1a9a1e2a2518a4e89fc883df8461f7ca9f578115
Anusha Kulkarni <88369729+hi-anusha@users.noreply.github.com>
2022-10-10 20:46:45 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (28.47%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -7,9 +7,9 @@ public:
             return dp[x][y];
        
         if(x>=m || y>=n)
-            return 0;
+            return dp[x][y]=0;
          if(x==m-1 && y==n-1)
-            return 1;
+            return dp[m][n]=1;
         return dp[x][y]=f(x+1,y,m,n,dp)+f(x,y+1,m,n,dp);
     }
     
