a8fd6bf340e35890432eb3630f1e91396c476abc
Anusha Kulkarni <88369729+hi-anusha@users.noreply.github.com>
2022-12-03 15:04:30 +0530

    Time: 3 ms (72.96%), Space: 9.6 MB (97.45%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -19,9 +19,9 @@ public:
         if(root==NULL)
             return NULL;
         swap(root->left,root->right);
-        TreeNode* left=invertTree(root->left);
-        TreeNode* right=invertTree(root->right);
-        
+        root->left=invertTree(root->left);
+        root->right=invertTree(root->right);
+     
         return root;
     }
 };
