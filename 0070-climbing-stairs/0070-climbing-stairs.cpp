6c7065a8f068a1bbd8b543913967a2c27b1185e1
Anusha Kulkarni <88369729+hi-anusha@users.noreply.github.com>
2022-12-12 20:37:24 +0530

    Time: 0 ms (100.00%), Space: 6 MB (55.72%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+         int prev1=1;
+        int prev2=1;
+        
+        int cur;
+        for(int i=2;i<=n;i++)
+        {
+            cur=prev1+prev2;
+            prev2=prev1;
+            prev1=cur;
+        }
+        return prev1;
+    }
+};
